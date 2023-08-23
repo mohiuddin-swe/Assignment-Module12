331e13a8caa0712152be2f99cf08cf224133b900
Mohiuddin <101384674+mohiuddin-swe@users.noreply.github.com>
2023-08-23 20:49:04 +0600

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,3 +1,13 @@
+import Image from 'next/image'
 export default function NotFoundPage() {
-  return <div>Page Not Found</div>;
+  return (
+    <div>Page Not Found</div>
+    <Image
+      src="/error.jpg"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+    
+  ) 
 }
